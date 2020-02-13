be4ddf5de5c4319500c3148a9daf8808128a485b
yuikotonoha <harutinkudo042@gmail.com>
2020-02-13 17:19:07 +0900

    [add]不要なファイルを作らないように設定、homesコントローラーを作成

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,14 @@ module HelloWorldRails
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+		# the framework and any gems in your application.
+
+		config.generators do |g|
+      g.javascripts false
+      g.stylesheets false
+      g.helper false
+      g.test_framework false
+    end
+
   end
 end
