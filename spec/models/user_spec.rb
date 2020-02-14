71c0c72214645621a350a728ea6a2de407f91a53
yuikotonoha <harutinkudo042@gmail.com>
2020-02-14 15:47:35 +0900

    [add]FactoryBot のメソッドを省略した形で呼び出す様に設定、userのfactory_botを作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,26 +3,24 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   context "account を指定しているとき" do
     it "ユーザーが作られる" do
-      user = User.new(name: "chinju", account: "chinju", email: "chinju@example.com")
+      user = create(:user)
       expect(user).to be_valid
     end
   end
 
   context "account を指定していないとき" do
     it "エラーする" do
-      user = User.new(name: "chinju", account: nil, email: "chinju@example.com")
+      user = build(:user, account: nil)
       user.valid?
-
       expect(user.errors.messages[:account]).to include "can't be blank"
     end
   end
 
   context "同名の account が存在するとき" do
     it "エラーする" do
-      User.create!(name: "chinju", account: "chinju", email: "chinju@example.com")
-      user = User.new(name: "chinju", account: "chinju", email: "chinju@example.com")
+      create(:user, account: "chinju")
+      user = build(:user, account: "chinju")
       user.valid?
-
       expect(user.errors.messages[:account]).to include "has already been taken"
     end
   end
