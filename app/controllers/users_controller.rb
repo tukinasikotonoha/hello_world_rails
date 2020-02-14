c2f6bda89d57f2c1b91bb81f46620e5676faedd9
yuikotonoha <harutinkudo042@gmail.com>
2020-02-14 11:32:01 +0900

    [add]usersのREAD処理を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
 	def index
 		@users = User.all
-		render json: @users
+  end
+
+	def show
+		binding.pry
+		@user = User.find(params[:id])
   end
 end
