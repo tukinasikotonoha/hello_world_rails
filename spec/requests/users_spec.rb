bc2a5454bf02a51b16844543c052e38ea11e981a
yuikotonoha <harutinkudo042@gmail.com>
2020-02-14 16:38:59 +0900

    [add]userモデル show と create のテストを実装

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,9 +2,59 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET /users" do
-    it "works! (now write some real specs)" do
-      get users_path
+    subject { get(users_path) }
+    before do
+      create_list(:user, 3)
+    end
+    it "ユーザーの一覧が取得できる" do
+      subject
+      res = JSON.parse(response.body)
+      expect(res.length).to eq 3
+      expect(res[0].keys).to eq ["account", "name", "email"]
       expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "GET /users/:id" do
+    subject { get(user_path(user_id)) }
+    context "指定した id のユーザーが存在する場合" do
+      let(:user) { create(:user) }
+      let(:user_id) { user.id }
+      it "ユーザーの値が取得できる" do
+        subject
+        res = JSON.parse(response.body)
+        expect(response).to have_http_status(200)
+
+        expect(res["account"]).to eq user.account
+        expect(res["name"]).to eq user.name
+        expect(res["email"]).to eq user.email
+      end
+    end
+
+    context "指定した id のユーザーが存在しない場合" do
+      let(:user_id) { 10000 }
+      it "ユーザーが見つからない" do
+        expect { subject }.to raise_error ActiveRecord::RecordNotFound
+      end
+    end
+  end
+
+  describe "POST /users" do
+    subject { post(users_path, params: params) }
+    let(:params) { { user: attributes_for(:user) } }
+    it "ユーザーのレコードが作成できる" do
+      expect { subject }.to change { User.count }.by(1)
+      expect(response).to have_http_status(204)
+    end
+  end
+
+  describe "PATCH /users/:id" do
+    it "任意のユーザーのレコードを更新できる" do
+    end
+  end
+
+  describe "DELETE /users/:id" do
+    it "任意のユーザーのレコードを削除できる" do
     end
   end
 end
