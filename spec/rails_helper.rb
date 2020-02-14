47d1dbd740f3f8bdfdacd99f46f91fee5efefce8
yuikotonoha <harutinkudo042@gmail.com>
2020-02-14 15:17:29 +0900

    [modify]rspec のテストファイルも同時に作成されるように設定

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,8 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+# Faker日本語化
+Faker::Config.locale = :ja
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -60,7 +62,4 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
 	# config.filter_gems_from_backtrace("gem name")
-
-	# Faker日本語化
-	Faker::Config.locale = :ja
 end
