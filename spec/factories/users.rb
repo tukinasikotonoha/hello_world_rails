2ea311d76582bd71cdaa8e274b7fb34859e95e9e
yuikotonoha <harutinkudo042@gmail.com>
2020-02-14 17:30:53 +0900

    [add]Factory で関連するデータも作れるように実装、 traitを導入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,10 @@ FactoryBot.define do
     name { Faker::Name.name }
     account { Faker::Internet.username }
     email { Faker::Internet.email }
+
+    trait :with_user_detail do
+      # association :user_detail, factory: :user_detail の省略形
+      user_detail
+    end
   end
 end
