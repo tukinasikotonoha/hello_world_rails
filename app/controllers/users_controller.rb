5314264986f3b3a9540c94c80d314cdab233729f
yuikotonoha <harutinkudo042@gmail.com>
2020-02-14 11:44:17 +0900

    [add]usersコントローラーにUpdataとDestroyの機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,27 @@
 class UsersController < ApplicationController
+	before_action :set_user, only: [:show, :update, :destroy]
+
 	def index
 		@users = User.all
   end
 
 	def show
-		binding.pry
-		@user = User.find(params[:id])
-  end
+	end
+
+	def update
+    @user.update!(user_params)
+	end
+
+	def destroy
+    @user.destroy!
+	end
+
+  private
+    def user_params
+      params.require(:user).permit(:account, :name, :email)
+		end
+
+		def set_user
+      @user = User.find(params[:id])
+    end
 end
