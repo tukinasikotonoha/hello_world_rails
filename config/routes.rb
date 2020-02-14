b00cff0fd550bea0e3441a5f7f280f12574de6d3
yuikotonoha <harutinkudo042@gmail.com>
2020-02-14 11:24:25 +0900

    [add]articlesのscaffoldを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :articles
   get 'users/index'
   get 'homes/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
